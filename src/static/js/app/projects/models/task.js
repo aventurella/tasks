bca373d73ca1703b49c6fef4a0e923ae11641f82
Adam Venturella <aventurella@gmail.com>
2013-11-30 20:30:04 -0800

    added description field

--- src/static/js/app/projects/models/task.js
+++ src/static/js/app/projects/models/task.js
@@ -5,6 +5,7 @@ var backbone = require('backbone');
 var Task = backbone.Model.extend({
     defaults: {
         label: null,
+        description: null,
         levelofEffort: 'medium'
     }
 });
