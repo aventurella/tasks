2bf507c6cee31d7b3e344d7e1f0f7508cbfbe54d
dinopetrone <dinopetrone@gmail.com>
2013-12-24 08:23:57 -0800

    updating defaults quick

--- src/static/js/app/settings/defaults.js
+++ src/static/js/app/settings/defaults.js
@@ -9,15 +9,13 @@ var settings = null;
 var ApplicationSettings = marionette.Controller.extend({
 
     getApiDomain: function(){
-        // 54.242.250.233 or localhost
-        // return 'http://54.242.250.233';
-        return 'http://localhost:8000';
+        return 'http://54.242.250.233';
+        // return 'http://localhost:8000';
     },
 
     getSocketDomain: function(){
-        // 54.242.250.233 or localhost
-        // return 'http://54.242.250.233:8888';
-        return 'http://localhost:8888';
+        return 'http://54.242.250.233:8888';
+        // return 'http://localhost:8888';
     },
 
     getToken: function(){
