02d59ddf29e9b0f6e85995e5720619430466ae50
Adam Venturella <aventurella@gmail.com>
2013-12-26 08:22:30 -0800

    mocking task creation

--- src/static/js/mock/tasks.js
+++ src/static/js/mock/tasks.js
@@ -1,26 +1,43 @@
-
 define(function(require, exports, module) {
+    var _ = require('underscore');
     require('jquery/mockjax');
 
     // gets all users for sidebar (testing function)
-    // admin/sidebar/collections/users.js
     $.mockjax({
-        url: '/api/v1/user/',
+        type: 'POST',
+        url: '/api/v1/task*',
         responseTime: 750,
-        data:{
-            search:'asdfasdf'
-        },
+
+        response: function(settings){
+            var data = JSON.parse(settings.data);
+            var id = _.uniqueId();
+
+            var response = {
+                'assigned_email': '',
+                'description': data.description,
+                'id': id,
+                'label': '[MOCK]' + data.label,
+                'loe': data.loe,
+                'project': data.project,
+                'resource_uri': '/api/v1/task/' + id + '/',
+                'status': data.status,
+                'task_type': data.task_type
+            };
+
+            this.responseText = response;
+        }
+    });
+
+    $.mockjax({
+        type: 'GET',
+        url: '/api/v1/project*',
+        responseTime: 750,
         responseText: {
-            ok: 'true',
-            data: [{
-                id: '1',
-                label: 'Dino Petrone',
-                first_name: 'Dino',
-                last_name: 'Petrone',
-                username: 'dinopetrone',
-                email: 'dinopetrone@blitzagency.com',
-                enabled: true
-            }]
+
+        'objects': [
+            {'id': 1, 'label': '[MOCK] Built', 'resource_uri': '/api/v1/project/1/'},
+            {'id': 3, 'label': '[MOCK] Tasks', 'resource_uri': '/api/v1/project/3/'}
+            ]
         }
     });
 });
