2b54b172b2d2487041c16c6180e00c79fdb77d27
Adam Venturella <aventurella@gmail.com>
2013-12-28 11:43:42 -0800

    wired up backlog quick button

--- src/static/js/app/projects/views/cells/backlog.js
+++ src/static/js/app/projects/views/cells/backlog.js
@@ -1,12 +1,16 @@
 define(function(require, exports, module) {
 
-var template = require('hbs!app/projects/templates/cell-backlog');
+var _ = require('underscore');
 var TaskView = require('./task').TaskView;
+var template = require('hbs!app/projects/templates/cell-backlog');
 
 var CellBacklogView = TaskView.extend({
     template: template,
     className: 'task backlog',
-    tag: 'backlog'
+    tag: 'backlog',
+    events: _.extend(TaskView.prototype.events, {
+        'click .btn-default.todo': 'wantsSetTodo'
+    })
 
 });
 
