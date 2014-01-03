da67e8dc7fe544773db6c181e91879aef131f0f3
dinopetrone <dinopetrone@gmail.com>
2014-01-02 22:16:14 -0800

    fixes task #71

--- src/static/js/app/projects/views/cells/in-progress.js
+++ src/static/js/app/projects/views/cells/in-progress.js
@@ -6,8 +6,11 @@ var TaskView = require('./task').TaskView;
 var CellInProgressView = TaskView.extend({
     template: template,
     className: 'task in-progress',
-    tag: 'in-progress'
-
+    tag: 'in-progress',
+    initialize: function(){
+        TaskView.prototype.initialize.apply(this, arguments);
+        this.listenTo(this.model, 'change:assigned_email', this.render);
+    }
 });
 
 exports.CellInProgressView = CellInProgressView;
